f215688db94017ff35e1d7c9f776212f69ceb80d
Mounish-1720 <mounishver2007@gmail.com>
2025-08-10 10:46:25 +0530

    Update courses.js

--- api/courses.js
+++ api/courses.js
@@ -9,6 +9,7 @@ module.exports = function handler(req, res) {
   const connection = snowflake.createConnection({
     account: process.env.SNOWFLAKE_ACCOUNT,
     username: process.env.SNOWFLAKE_USERNAME,
+    password: process.env.SNOWFLAKE_PASSWORD,
     database: process.env.SNOWFLAKE_DATABASE,
     schema: process.env.SNOWFLAKE_SCHEMA,
     role: process.env.SNOWFLAKE_ROLE
